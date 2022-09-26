5b920ff9de9fec30b0c3f64844254c9f57bcfe48
Vinicius Rocha <37674676+ViniciusRock@users.noreply.github.com>
2022-09-25 23:25:39 -0300

    Nova versão do TCC
    Nova versão do TCC

--- js/index.js
+++ js/index.js
@@ -1,28 +1,4 @@
-console.log("primeiro console log")
-/*CRIAR validação da matrícula,CPF, nome(iniciais em maiúsculo SE POSSÍVEL!), celular,Siape,DEFINIR LIMITE DE SENHA*/
-
-//Constantes
-const cpf = document.querySelector('#cpf');
-const data = document.querySelector('#data');
-const celular = document.querySelector('#celular')
-//Validando CPF
-cpf.addEventListener('keypress', () => {
-    let inputlength = cpf.value.length;
-    //no cpf tem: 3 números+ 1 ponto, 3 números + 1 ponto, tracinho 2 números
-    if (inputlength === 3 || inputlength === 7) {
-        cpf.value += '.';
-    } else if (inputlength === 11) {
-        cpf.value += '-';
-    }
-})
-
-//Validando Data
-data.addEventListener('keypress', ()=>{
-    let datalength = data.value.length;
-    if(datalength == 2 || datalength == 5){
-        data.value += '/';
-    }
-})
+const celular = document.getElementById('celular')
 
 //Validando celular
 celular.addEventListener('keypress', () => {
@@ -30,7 +6,16 @@ celular.addEventListener('keypress', () => {
     if (celularlength == 2) {
         celular.value += " "
     }
-    if (celularlength == 10) {
+    if (celularlength == 6 || celularlength == 10) {
         celular.value += "-"
     }
 })
+function mostrarOcultarSenha(){
+    var senha = document.getElementById("senha");
+    if(senha.type=="password"){
+        senha.type = "text";
+    }
+    else{
+        senha.type="password";
+    }
+}
---
+++ login/login.js
@@ -0,0 +1,7 @@
+     const btn = document.querySelector(".btn");
+        
+
+        btn.addEventListener("click",()=>{
+       
+            document.body.classList.toggle("active")
+        })
